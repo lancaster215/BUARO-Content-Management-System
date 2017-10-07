14457151cf6c53cbebd8daf3e6b8b6eae1b28727
lancaster215 <francisco.ifurung@bicol-u.edu.ph>
2017-10-07 16:51:10 +0800

    Add files via upload

---
+++ .event-recent.php
@@ -0,0 +1,88 @@
+<?php
+	include '../connection.php';
+	include '../../../vendor/Parsedown/Parsedown.php';
+	require_once '../../../admin/php/backend/input_handler.php';
+?>
+
+<style>
+.description p{
+	max-height: 120px;
+}
+
+.pic img {
+  height: 150px;
+  width:150px;
+  border-radius:50%;
+
+ 
+  -webkit-transition: all 1s ease;
+     -moz-transition: all 1s ease;
+       -o-transition: all 1s ease;
+      -ms-transition: all 1s ease;
+          transition: all 1s ease;
+
+}
+ 
+.pic img:hover {
+  box-shadow: 0px 0px 15px 5px rgba(65, 65, 65, 0.99);
+}
+.current {
+  color: green;
+}
+
+#pagin li {
+  display: inline-block;
+}
+
+</style>
+
+<link rel="stylesheet" type="text/css" href="../css/breadcrumb.css">
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script  src="../js/jquery_pagination.js"></script>
+<div id="content2">												<!--  T  H  E     C  O  N  T  E  N  T  -->
+			<div class="top-b">	
+				<ul class="breadcrumbs">
+					<a class="w3-padding-16 w3-opennav" href="javascript:void(0)" onclick="w3_open()" style="text-decoration:none;margin-left:40px;margin-right:10px;font-size:20px;">&#9776;</a>
+					<li><a href="aro3.php">Home</a></li>
+					<li>Recent Events || <a href="#" onclick="loadStories()">Stories</a></li>
+				</ul>
+			</div>	
+			<div class="w3-container w3-padding-jumbo">
+				
+				<?php
+					$parsedown = new Parsedown();
+					$getquery = "SELECT * FROM post WHERE post_type = 2  AND status = 'shown'";
+					$getquery .= "ORDER BY eventdate DESC";
+					$run = mysqli_query($con,$getquery);
+					$id = 0;
+					$today = date("Y-m-d");
+					while($row = mysqli_fetch_array($run)){
+						$newstring = decode(substr($parsedown->text($row['content']),0,250));
+						if($row['eventdate'] < $today){
+						if(!isset($row['imgbanner']) || $row['imgbanner']=="none" || $row['imgbanner']==""){
+							$row['imgbanner'] = "../../data/events-stories/noimage.jpg";
+						}
+
+						echo '
+						<div class="pic" onclick="viewEvent('.$row['post_id'].')">
+							<div class="w3-container w3-half line-content">
+								<h5 class="title"><b>Event Date: '.date("M d, Y",strtotime($row['eventdate'])).'</b></h5>						
+								<h4>'.$row['title'].'</h4>
+								<div class="w3-container w3-half" style="margin-bottom:50px;">	
+									<img src="'.$row['imgbanner'].'" alt = "Avatar">
+								</div>
+								<div class="w3-container description" style="max-width:500px;">		
+	  								<p>'.decode($newstring).'<a style="cursor:pointer; color:#441cff;">...Read More</a></p>
+	  							</div>
+	  							<hr>
+	  						</div>
+	  						<ul id="pagin">
+	  						</ul>
+						</div>
+							';
+						$id++;
+					}
+					}
+				?>
+				
+			</div>
---
+++ .event.php
@@ -0,0 +1,120 @@
+<?php
+	include '../connection.php';
+	include '../../vendor/Parsedown/Parsedown.php';
+	require_once '../../../admin/php/backend/input_handler.php';
+
+	$id = $_GET['id'];
+	$query = "SELECT * FROM post WHERE post_id='$id'";
+	$run = mysqli_query($con,$query);
+
+	$fetch = mysqli_fetch_array($run);
+?>
+<link rel="stylesheet" type="text/css" href="../css/breadcrumb.css">
+<div>	
+											<!--  T  H  E     C  O  N  T  E  N  T  -->
+			<div class="w3-container w3-padding-jumbo">
+			<div class="top-b">
+				<ul class="breadcrumbs">
+					<a class="w3-padding-16 w3-opennav" href="javascript:void(0)" onclick="w3_open()" style="text-decoration:none;margin-left:40px;margin-right:10px;font-size:20px;">&#9776;</a>
+				  <li><a href="aro3.php">Home</a></li>
+				  <li>Events</li>
+				  <li><?php echo $fetch['title']?></li>
+				</ul>
+			</div>
+				<?php
+					if(!isset($fetch['imgbanner']) || $fetch['imgbanner']=="none" || $fetch['imgbanner']==""){
+							$fetch['imgbanner'] = "../../data/events-stories/noimage.jpg";
+						}	
+
+					if($fetch['imglinks']!=""){
+						echo '
+							<div class="container">
+							<div id="myCarousel-'.$fetch['post_id'].'" class="carousel slide" data-ride="carousel">
+							  <!-- Wrapper for slides -->
+							  <div class="carousel-inner">
+							';
+
+						$nostr = explode(";", $fetch['imglinks']);
+						$q=0;
+							echo '<ol class="carousel-indicators">';
+						foreach($nostr as $z){
+								if($q==0)
+									echo ' <li data-target="#myCarousel-'.$fetch['post_id'].'" data-slide-to="'.$q.'" class="active"></li>';
+								else
+									echo '<li data-target="#myCarousel-'.$fetch['post_id'].'" data-slide-to="'.$q.'"></li>';
+								$q++;
+						}
+						echo '</ol>';
+							$strings = explode(";", $fetch['imglinks']);
+							$q=1;
+							foreach ($strings as $links) {
+								if($q==1){
+									echo '
+										<div class="item active">
+											<img class="img-rounded" src="../admin/img/'.$links.'" alt="img-'.$links.'">
+										</div>
+									';
+									$q++;
+								}
+									else{
+										echo '
+											<div class="item">
+											  <img class="img-rounded" src="../admin/img/'.$links.'" alt="img-'.$links.'">
+											</div>
+										';
+									}
+
+								}
+
+								echo '
+									</div>
+									  <!-- Left and right controls -->
+									  <a class="left carousel-control" href="#myCarousel-'.$fetch['post_id'].'" data-slide="prev">
+										<span class="glyphicon glyphicon-chevron-left"></span>
+										<span class="sr-only">Previous</span>
+									  </a>
+									  <a class="right carousel-control" href="#myCarousel-'.$fetch['post_id'].'" data-slide="next">
+										<span class="glyphicon glyphicon-chevron-right"></span>
+										<span class="sr-only">Next</span>
+									  </a>
+									</div>
+									</div>
+								';
+						}
+						else{
+							echo '
+									<div class="container">
+									<div id="emptyCarousel-'.$fetch['post_id'].'" class="carousel slide" data-ride="carousel">
+									  <!-- Indicators -->
+									  <ol class="carousel-indicators">
+										<li data-target="#emptyCarousel-'.$fetch['post_id'].'" data-slide-to="0" class="active"></li>
+									  </ol>
+
+									  <!-- Wrapper for slides -->
+									  <div class="carousel-inner">
+										<div class="item active">
+										  <center><img src="../../data/events-stories/noslider.jpg" alt="no images"></center>
+										</div>
+									  </div>
+
+									  <!-- Left and right controls -->
+									  <a class="left carousel-control" href="#emptyCarousel-'.$fetch['post_id'].'" data-slide="prev">
+										<span class="glyphicon glyphicon-chevron-left"></span>
+										<span class="sr-only">Previous</span>
+									  </a>
+									  <a class="right carousel-control" href="#emptyCarousel-'.$fetch['post_id'].'" data-slide="next">
+										<span class="glyphicon glyphicon-chevron-right"></span>
+										<span class="sr-only">Next</span>
+									  </a>
+									</div>
+								</div>
+							';
+						}				
+					?>					
+				<h1 class="title"><?php echo $fetch['title']; ?></h1><br>
+				<h6><b>Event Date: <?php echo date("Y-M-d",strtotime($fetch['eventdate']))?></b></h6>
+				<img src="../../data/events-stories/noimage.jpg" >
+				<h5><?php
+						$parsedown = new Parsedown(); 
+						echo decode($parsedown->text( $fetch['content']));?></h5>
+			</div>
